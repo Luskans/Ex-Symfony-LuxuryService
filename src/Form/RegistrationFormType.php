63d07ad378944790dc31c7a8f507481b6f244b3c
Luskans <sylvain.migne@gmail.com>
2023-09-20 17:00:28 +0200

    login, register and links done

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,7 +5,9 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -18,32 +20,131 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+                'attr' => [
+                    'id' => 'email',
+                    'required' => true,
+                    // 'data-parsley-trigger' => 'change',
+                    // 'data-parlsey-error-message' => 'A valid email address is required.'
                 ],
+                // 'help' => 'Type you email address.'
             ])
-            ->add('plainPassword', PasswordType::class, [
+            // ->add('plainPassword', PasswordType::class, [
+            //     // instead of being set onto the object directly,
+            //     // this is read and encoded in the controller
+            //     'mapped' => false,
+            //     'attr' => ['autocomplete' => 'new-password'],
+            //     'constraints' => [
+            //         new NotBlank([
+            //             'message' => 'Please enter a password',
+            //         ]),
+            //         new Length([
+            //             'min' => 6,
+            //             'minMessage' => 'Your password should be at least {{ limit }} characters',
+            //             // max length allowed by Symfony for security reasons
+            //             'max' => 4096,
+            //         ]),
+            //     ],
+            // ])
+            // ->add('password', PasswordType::class, [
+            //     // instead of being set onto the object directly,
+            //     // this is read and encoded in the controller
+            //     'mapped' => false,
+            //     'attr' => [
+            //         'autocomplete' => 'new-password',
+            //         'id' => 'password'
+            //     ],
+            //     'constraints' => [
+            //         new NotBlank([
+            //             'message' => 'Please enter a password',
+            //         ]),
+            //         new Length([
+            //             'min' => 6,
+            //             'minMessage' => 'Your password should be at least {{ limit }} characters',
+            //             // max length allowed by Symfony for security reasons
+            //             'max' => 4096,
+            //         ]),
+            //     ],
+            // ])
+            // ->add('password_confirmation', PasswordType::class, [
+            //     // instead of being set onto the object directly,
+            //     // this is read and encoded in the controller
+            //     'mapped' => false,
+            //     'attr' => [
+            //         // 'autocomplete' => 'new-password',
+            //         'id' => 'password_confirmation'
+            //     ],
+            //     'constraints' => [
+            //         new NotBlank([
+            //             'message' => 'Please enter a password',
+            //         ]),
+            //         new Length([
+            //             'min' => 6,
+            //             'minMessage' => 'Your password should be at least {{ limit }} characters',
+            //             // max length allowed by Symfony for security reasons
+            //             'max' => 4096,
+            //         ]),
+            //     ],
+            // ])
+            ->add('password', RepeatedType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
+                'type' => PasswordType::class,
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
-                'constraints' => [
-                    new NotBlank([
-                        'message' => 'Please enter a password',
-                    ]),
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 4096,
-                    ]),
+                
+                'options' => [
+                    'attr' => [
+                        // 'class' => 'password-field',
+                        'autocomplete' => 'new-password',
+                        // 'required' => true,
+                        // 'data-parsley-trigger' => 'change',
+                        // 'data-parsley-minlength' => '6',
+                        // 'data-parsley-error-message' => 'The password must be at least 6 characters.'
+                    ],
+                    
+                ],
+                
+                'first_options'  => [
+                    'label' => 'Password',
+                    'attr' => [
+                        'id' => 'password',
+                    ],
+                    'constraints' => [
+                        new NotBlank([
+                            'message' => 'Please enter a password',
+                        ]),
+                        new Length([
+                            'min' => 6,
+                            'minMessage' => 'Your password should be at least {{ limit }} characters',
+                            // max length allowed by Symfony for security reasons
+                            'max' => 4096,
+                        ]),
+                    ],
+                ],
+                'second_options' => [
+                    'label' => 'Confirm password',
+                    'attr' => [
+                        'id' => 'password_confirmation',
+                    ],
+                    // 'invalid_message' => 'The password fields must match.',
                 ],
             ])
+
+            // ->add('agreeTerms', CheckboxType::class, [
+            //     'mapped' => false,
+            //     'constraints' => [
+            //         new IsTrue([
+            //             'message' => 'You should agree to our terms.',
+            //         ]),
+            //     ],
+            //     'attr' => [
+            //         'value' => '1',
+            //         'id' => 'agreeTerms',
+            //         'required' => true,
+            //     ],
+            //     // 'label' => 'I have read and I accept the <a href="#!" target="_blank">Terms Of Use</a>',
+            // ])
         ;
     }
 
